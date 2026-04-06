c855928c14a7f6896aab1c57354e4e083bd18e5e
C/kadir <ccmboyccmboy7@gmail.com>
2026-04-06 21:43:07 +0300

    Smile House Port: First Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const canshurta_Headers=document.querySelector("#tax");
+const abka=document.querySelector("#maga-markabka-input");
+const misanka=document.querySelector("#misanka");
+const shidalka=document.querySelector("#shidalka");
+const nuuca_alabta=document.querySelector("#nuuca-alabta");
+const submit=document.querySelector("#submit");
+const booshaye=document.querySelector("#booshaye");
